294421181fc8b64950f0b2fcb05962d948d44456
Yuta Matsuo <y.matsuo.dev@gmail.com>
2023-07-14 23:07:00 +0900

    add site icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,15 @@ export const metadata: Metadata = {
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang='ja'>
+      <head>
+        <link rel='apple-touch-icon' sizes='180x180' href='/favicons/apple-touch-icon.png' />
+        <link rel='icon' type='image/png' sizes='32x32' href='/favicons/favicon-32x32.png' />
+        <link rel='icon' type='image/png' sizes='16x16' href='/favicons/favicon-16x16.png' />
+        <link rel='manifest' href='/favicons/site.webmanifest' />
+        <link rel='mask-icon' href='/favicons/safari-pinned-tab.svg' color='#5bbad5' />
+        <meta name='msapplication-TileColor' content='#00aba9' />
+        <meta name='theme-color' content='#ffffff' />
+      </head>
       <body className={inter.className}>
         <AuthProvider>{children}</AuthProvider>
       </body>
