2dadbd1e9fdee67d6b4d16b3bbef3f0441db380b
Yuta Matsuo <y.matsuo.dev@gmail.com>
2023-07-16 09:22:05 +0900

    add file size validation

--- src/components/pages/profile/Edit/index.tsx
+++ src/components/pages/profile/Edit/index.tsx
@@ -36,9 +36,15 @@ export const Edit: FC<Props> = ({ profileData, profileIconUrl }) => {
   const handleUploadImage = async (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files === null || e.target.files.length === 0) return;
 
+    const file = e.target.files[0];
+    if (file.size > 5 * 1000 * 1000) {
+      openToast('error', '5MB以下のみ登録可能です');
+      return;
+    }
+
     try {
       const body = new FormData();
-      body.append('iconFile', e.target.files[0]);
+      body.append('iconFile', file);
 
       const profileId = profileFormMethods.getValues('id');
       const res = await fetch(`/api/admin/profile/${profileId}/icon`, {
