dbd6fb7ecd7f3a700a7fa318eeb05d02e087b63b
Yuta Matsuo <y.matsuo.dev@gmail.com>
2023-07-18 07:28:39 +0900

    fix metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,11 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'Mr.Pricer',
   description: 'I`m Mr.Pricer. I`ll help you to find the best price.',
+  viewport: {
+    width: 'device-width',
+    initialScale: 1,
+    maximumScale: 1,
+  },
   openGraph: {
     url: 'https://www.mr-pricer.com',
     type: 'website',
@@ -25,20 +30,32 @@ export const metadata: Metadata = {
     description: 'I`m Mr.Pricer. I`ll help you to find the best price.',
     images: ['https://www.mr-pricer.com/images/ogp.png'],
   },
+  icons: {
+    icon: [
+      { url: '/favicons/favicon-32x32.png', type: 'image/png', sizes: '32x32' },
+      { url: '/favicons/favicon-16x16.png', type: 'image/png', sizes: '16x16' },
+    ],
+    apple: [{ sizes: '180x180', url: '/favicons/apple-touch-icon.png' }],
+    other: [
+      {
+        rel: 'manifest',
+        url: '/favicons/site.webmanifest',
+      },
+      {
+        rel: 'mask-icon',
+        url: '/favicons/safari-pinned-tab.svg',
+      },
+    ],
+  },
+  themeColor: '#ffffff',
+  other: {
+    msapplicationTileColor: '#00aba9',
+  },
 };
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang='ja'>
-      <head>
-        <link rel='apple-touch-icon' sizes='180x180' href='/favicons/apple-touch-icon.png' />
-        <link rel='icon' type='image/png' sizes='32x32' href='/favicons/favicon-32x32.png' />
-        <link rel='icon' type='image/png' sizes='16x16' href='/favicons/favicon-16x16.png' />
-        <link rel='manifest' href='/favicons/site.webmanifest' />
-        <link rel='mask-icon' href='/favicons/safari-pinned-tab.svg' color='#5bbad5' />
-        <meta name='msapplication-TileColor' content='#00aba9' />
-        <meta name='theme-color' content='#ffffff' />
-      </head>
       <body className={inter.className}>
         <AuthProvider>{children}</AuthProvider>
       </body>
