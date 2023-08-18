8ecef52ff2e71fda3efa8ef14dc82f1ea22344c2
Yuta Matsuo <y.matsuo.dev@gmail.com>
2023-08-19 06:21:21 +0900

    利用規約（仮）

---
+++ src/app/(publish-static)/terms/page.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Mr.Pricer - 利用規約',
+  description: 'Mr.Pricer - 利用規約',
+};
+
+const Terms = async () => {
+  return (
+    <div>
+      <section>
+        <h1>Mr.Pricer ベータ版 利用規約</h1>
+        <p>
+          この利用規約（以下「本規約」といいます）は、Mr.Pricer（以下「当社」といいます）が提供するベータ版WEBサービス（以下「本サービス」といいます）の利用に関する条件を定めるものです。本サービスの利用者（以下「利用者」といいます）は、本規約に同意の上で本サービスをご利用いただきます。
+        </p>
+
+        <h2>1. サービスの提供</h2>
+        <ol>
+          <li></li>
+        </ol>
+      </section>
+    </div>
+  );
+};
+
+export default Terms;
